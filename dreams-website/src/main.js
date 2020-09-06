81f229349e6c1187b4e5f5eb8118f8d4c7519882
Jakub Erazmus <jerazmus@o2.pl>
2020-09-06 20:32:38 +0200

    added vue-router and additional components

--- dreams-website/src/main.js
+++ dreams-website/src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './routes/index'
 
-// Install BootstrapVue
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
